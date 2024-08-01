d0a2d14a0e580d805e3bf77d2c7165376ae27da9
AtomicFive4059 <mshelke074@gmail.com>
2024-08-01 12:07:30 +0530

    Backend Java Code for Male-Female RelativeLayout

--- app/src/main/java/com/example/bmiapp/MainActivity.java
+++ app/src/main/java/com/example/bmiapp/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -19,6 +23,19 @@ import androidx.core.view.WindowInsetsCompat;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    int intWeight = 55;
+    int intAge = 22;
+    int currentProgress;
+
+    String intProgress = "150";
+    String typeOfUsers = "0";
+    String weight = "55";
+    String age = "22";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +52,71 @@ public class MainActivity extends AppCompatActivity {
 
         Button button =findViewById(R.id.calculateBMIBtn);
 
-        TextView textView = findViewById(R.id.txtMale);
+        TextView txtCurrentHeight = findViewById(R.id.currentHeight);
+        TextView txtCurrentWeight = findViewById(R.id.currentWeight);
+        TextView txtMale = findViewById(R.id.txtMale);
+        TextView txtFemale = findViewById(R.id.txtFemale);
+
+        ImageView imgIncreamentWeight = findViewById(R.id.increamentWeight);
+        ImageView imgDecreamentWeighyt = findViewById(R.id.decreamentWeight);
+        ImageView imgIncreamentAge = findViewById(R.id.increamentAge);
+        ImageView imgDecreamentAge = findViewById(R.id.decreamentAge);
+
+        SeekBar seekBarForHeight = findViewById(R.id.seekbarForHeight);
+
+        RelativeLayout male = findViewById(R.id.male);
+        RelativeLayout female = findViewById(R.id.female);
+
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                male.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.malefemale_focus));
+              //  txtMale.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left));
+                female.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.heightcard_bg));
+                typeOfUsers = "Male";
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                female.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.malefemale_focus));
+              //  txtFemale.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left));
+                male.setBackground(ContextCompat.getDrawable(getApplicationContext(),R.drawable.heightcard_bg));
+                typeOfUsers = "Female";
+            }
+        });
+
+//
+//        seekBarForHeight.setMax(300);
+//        seekBarForHeight.setProgress(150);
+//
+//        seekBarForHeight.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+//            @Override
+//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                currentProgress = progress;
+//                intProgress = String.valueOf(currentProgress);  //Type casting Int to String
+//                txtCurrentHeight.setText(intProgress);
+//            }
+//
+//            @Override
+//            public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//            }
+//        });
+
+
+
+
+
+
+        //TextView textView = findViewById(R.id.txtMale);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,9 +124,9 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MainActivity.this,ShowBMI.class);
                 startActivity(intent);
 
-
-               // textView.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left));     it's work..!!
+                // textView.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.slide_in_left));     it's work..!!
             }
         });
+
     }
 }

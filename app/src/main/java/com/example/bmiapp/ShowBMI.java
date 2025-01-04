31dbca0b1cdb1ed429a71c2ca0b869956567ccc1
AtomicFive4059 <mshelke074@gmail.com>
2025-01-04 12:19:27 +0530

    WebView link of App data

--- app/src/main/java/com/example/bmiapp/ShowBMI.java
+++ app/src/main/java/com/example/bmiapp/ShowBMI.java
@@ -1,5 +1,6 @@
 package com.example.bmiapp;
 
+import android.Manifest;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -8,10 +9,13 @@ import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.window.OnBackInvokedDispatcher;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -47,14 +51,24 @@ public class ShowBMI extends AppCompatActivity {
         ImageView imgDisplay = findViewById(R.id.resultImg);
         RelativeLayout background = findViewById(R.id.contentLayout);
 
+        LinearLayout linearLayoutLink1 = findViewById(R.id.link1);
+
         Button button =findViewById(R.id.recalculateBMIBtn);
 
 
           Objects.requireNonNull(getSupportActionBar()).setElevation(0);
           getSupportActionBar().setTitle(Html.fromHtml("<font color=\"white\"></font>"));
           getSupportActionBar().setTitle("Result is..");
+
         ColorDrawable colorDrawable = new ColorDrawable(getColor(R.color.black));
         getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
+        linearLayoutLink1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ShowBMI.this,BasedOn1.class));
+            }
+        });
 
         Intent intent;
         intent = getIntent();
@@ -71,30 +85,32 @@ public class ShowBMI extends AppCompatActivity {
 
         bmi = Float.toString(intbmi);
 
-        if (intbmi<16){
-            bmiCategory.setText("Severe Thiness");
+        if (intbmi<18.5){
+            bmiCategory.setText("Underweight");
            // background.setBackgroundColor(Color.RED);
-            imgDisplay.setImageResource(R.drawable.thinness);
-        } else if (intbmi<16.9 && intbmi>16) {
-            bmiCategory.setText("Moderate Thinness");
+            imgDisplay.setImageResource(R.drawable.underweight);
+        } else if (intbmi<18.5 && intbmi>24.9) {
+            bmiCategory.setText("Optimum Range");
           //  background.setBackgroundColor(Color.BLUE);
-            imgDisplay.setImageResource(R.drawable.moderatethin);
-        }else if (intbmi<18.4 && intbmi>17) {
-            bmiCategory.setText("Mile Thinness");
-         //   background.setBackgroundColor(Color.YELLOW);
-            imgDisplay.setImageResource(R.drawable.milethine);
-        }else if (intbmi<25 && intbmi>18.4) {
-            bmiCategory.setText("Normal");
-           // background.setBackgroundColor(Color.GREEN );
             imgDisplay.setImageResource(R.drawable.normal);
-        }else if (intbmi<29.4 && intbmi>25) {
-            bmiCategory.setText("OverWeight");
-          //  background.setBackgroundColor(getResources().getColor(R.color.green));
-            imgDisplay.setImageResource(R.drawable.obesity);
-        }else {
-            bmiCategory.setText("Obese Class I");
+        }else if (intbmi<25 && intbmi>29.9) {
+            bmiCategory.setText("Overweight");
+         //   background.setBackgroundColor(Color.YELLOW);
+            imgDisplay.setImageResource(R.drawable.overweight);
+        }else if (intbmi<30 && intbmi>34.9) {
+            bmiCategory.setText("Obese");
+           // background.setBackgroundColor(Color.GREEN );
+            imgDisplay.setImageResource(R.drawable.obese);
+        }
+//        else if (intbmi<35 && intbmi>39.9) {
+//            bmiCategory.setText("Extremly Obese");
+//          //  background.setBackgroundColor(getResources().getColor(R.color.green));
+//            imgDisplay.setImageResource(R.drawable.extremly_obese);
+//        }
+        else {
+            bmiCategory.setText("Extremly Obese");
            // background.setBackgroundColor(getResources().getColor(R.color.red));
-            imgDisplay.setImageResource(R.drawable.obesity);
+            imgDisplay.setImageResource(R.drawable.extremly_obese);
         }
 
         txtgenderDisplay.setText(intent.getStringExtra("gender"));
@@ -109,8 +125,16 @@ public class ShowBMI extends AppCompatActivity {
                //calling startActivity method here
                startActivity(intent);
                //lastly calling finished method to destroy ShowBMI Activity
-               finish();
+              // finish();
            }
        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+//        on the back presses of the user, we jumped to the MainActivity.java
+        startActivity(new Intent(ShowBMI.this, MainActivity.class));
     }
 }
